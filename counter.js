9c92a10a092dab7265903d9824ee09d0c43ee53a
vladimirnetworks <47903621+vladimirnetworks@users.noreply.github.com>
2023-11-16 11:53:58 +0330

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,5 +1,6 @@
 mydiv = document.getElementsByTagName("h1")[0]
 count = 1;
+mydiv.style.transition="all 0.5s"
 setInterval(()=>{
   mydiv.innerHTML = "BEN "+count ;
   mydiv.style.fontSize = count*0.5+"rem"
